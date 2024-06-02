ad4cf0179f09a74c55bef8f75e787894a97cd09c
Shunt <shunt_ps@hotmail.com>
2024-06-02 15:22:11 -0400

    Add main app, begin db and serializers

---
+++ backend/main/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from . import models
+
+
+class VendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Vendor
+        fields = ['user', 'address']
---
+++ backend/main/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from . import serializers
+from . import models
+
+
+class VendorList(generics.ListAPIView):
+    queryset = models.Vendor.objects.all()
+    serializer_class = serializers.VendorSerializer
