f2250904a62a055fb86523b64826885b5282e9f9
Shunt <shunt_ps@hotmail.com>
2024-06-03 16:56:49 -0400

    Refactor serializers to set Meta.depth directly in the Meta class

--- backend/main/serializers.py
+++ backend/main/serializers.py
@@ -6,37 +6,25 @@ class VendorListSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Vendor
         fields = ['id', 'user', 'address']
-
-    def __init__(self, *args, **kwargs):
-        super(VendorListSerializer, self).__init__(*args, **kwargs)
-        self.Meta.depth = 1
+        depth = 1
 
 
 class VendorDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Vendor
         fields = ['id', 'user', 'address']
-
-    def __init__(self, *args, **kwargs):
-        super(VendorDetailSerializer, self).__init__(*args, **kwargs)
-        self.Meta.depth = 1
+        depth = 1
 
 
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Product
         fields = ['id', 'category', 'vendor', 'title', 'detail', 'price']
-
-    def __init__(self, *args, **kwargs):
-        super(ProductListSerializer, self).__init__(*args, **kwargs)
-        self.Meta.depth = 1
+        depth = 1
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Product
         fields = ['id', 'category', 'vendor', 'title', 'detail', 'price']
-
-    def __init__(self, *args, **kwargs):
-        super(ProductDetailSerializer, self).__init__(*args, **kwargs)
-        self.Meta.depth = 1
+        depth = 1
